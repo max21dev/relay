cc57503138099c147b1275c82531fad13ce615f7
max21dev <max21dev@gmail.com>
2024-08-19 22:56:55 +0200

    add privacy and group type field to create group page

--- examples/groups.nip29.com/pages.go
+++ examples/groups.nip29.com/pages.go
@@ -19,6 +19,21 @@ func homepageHTML() HTMLComponent {
 				Input("").Id("npub").Placeholder("npub1...").Name("pubkey").Class("w-96 px-4 py-2 outline-0 bg-stone-100"),
 				Label("group name:").For("name").Class("mr-1 mt-4 block"),
 				Input("").Id("name").Placeholder("my little group").Name("name").Class("w-96 px-4 py-2 outline-0 bg-stone-100"),
+
+				// Adding the privacy field with Disabled
+				Label("privacy:").For("privacy").Class("mr-1 mt-4 block"),
+				Select(
+					Option("public").Value("public"),
+					Option("private").Value("private"),
+				).Id("privacy").Name("privacy").Class("w-96 px-4 py-2 outline-0 bg-stone-100").Disabled(true),
+
+				// Adding the type field with Disabled
+				Label("type:").For("type").Class("mr-1 mt-4 block"),
+				Select(
+					Option("open").Value("open"),
+					Option("closed").Value("closed"),
+				).Id("type").Name("type").Class("w-96 px-4 py-2 outline-0 bg-stone-100").Disabled(true),
+
 				Button("create").Class("block rounded mt-4 px-4 py-2 bg-emerald-500 text-white hover:bg-emerald-300 transition-colors"),
 			).Action("/create").Method("POST"),
 		).Class("mx-4 my-6"),
